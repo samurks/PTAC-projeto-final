97ddda7e243d83e0890539d9e4d81ccba9e16375
samurks <samueltorochagas@gmail.com>
2023-12-11 00:07:15 -0300

    adicionei propriedades para o texto usando css

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Card.css';
 
 export default function Card({ ytb }) {
+  const urlId = ytb.url.slice(17);
 
-    const urlId = ytb.url.slice(17)
-
-    return (
-        <div><Link to = {`detalhe/${ytb.id}`}>
-            <h1>{ytb.video}</h1>
-            </Link>
-            <iframe
-                width="300"
-                height="200"
-                src={`https://www.youtube.com/embed/${urlId}`}
-                title="Vídeo"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen>
-            </iframe>
-
-        </div>
-    );
-
-};
+  return (
+    <div>
+      <Link className="corTitulo" to={`detalhe/${ytb.id}`}>
+        <h1>{ytb.video}</h1>
+      </Link>
+      <iframe
+        width="300"
+        height="200"
+        src={`https://www.youtube.com/embed/${urlId}`}
+        title="Vídeo"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
