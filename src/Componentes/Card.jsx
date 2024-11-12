82b416968a1d472dc345f5b987e196d288845ab2
samuel <samueltorochagas@gmail.com>
2024-11-12 10:20:51 -0400

    change to github pages

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -3,7 +3,13 @@ import { Link } from 'react-router-dom';
 import './Card.css';
 
 export default function Card({ ytb }) {
-  const urlId = ytb.url.slice(17);
+  const getYouTubeId = (url) => {
+    const match = url.match(/(?:https?:\/\/)?(?:www\.)?youtube\.com\/(?:watch\?v=|embed\/|v\/|.+\?v=)?([^&\n?#]+)/) ||
+                  url.match(/(?:https?:\/\/)?(?:www\.)?youtu\.be\/([^&\n?#]+)/);
+    return match ? match[1] : '';
+  };
+
+  const urlId = getYouTubeId(ytb.url);
 
   return (
     <div>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PTAC-projeto-final/', // Adicione o nome do repositório como base
 })
