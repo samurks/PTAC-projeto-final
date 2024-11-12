d566b65b02499f9c1acd0e21a3740364c0148dd6
samuel <samueltorochagas@gmail.com>
2024-11-12 10:23:59 -0400

    clear comments

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PTAC-projeto-final/', // Adicione o nome do repositório como base
+  base: '/PTAC-projeto-final/',
 })
